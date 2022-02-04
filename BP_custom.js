004a80a2453574ea152f6423054cef7001e3c7f7
Lou <whereislou@gmail.com>
2022-02-04 12:03:27 +1100

    Update BP_custom.js

--- BP_custom.js
+++ BP_custom.js
@@ -1,19 +1,21 @@
 
     //////BANNER ROTATION /////
     
-    var slideIndex = 0;
-    carousel();
-    
-    function carousel() {
-        var i;
-        var x = document.getElementsByClassName("PromotionBanner-listItem-text");
-        for (i = 0; i < x.length; i++) {
-            x[i].style.display = "none";
-        }
-        slideIndex++;
-        if (slideIndex > x.length) {
-            slideIndex = 1
-        }
-        x[slideIndex - 1].style.display = "block";
-        setTimeout(carousel, 5000);
-    }
+    $(document).ready(function() {
+        var slideIndex = 0;
+        carousel();
+        
+        function carousel() {
+            var i;
+            var x = document.getElementsByClassName("PromotionBanner-listItem-text");
+            for (i = 0; i < x.length; i++) {
+                x[i].style.display = "none";
+            }
+            slideIndex++;
+            if (slideIndex > x.length) {
+                slideIndex = 1
+            }
+            x[slideIndex - 1].style.display = "block";
+            setTimeout(carousel, 5000);
+        };
+    });
