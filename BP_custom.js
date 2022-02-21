4f721904f9e97a30e00d3abc50d28937be18b744
Lou <whereislou@gmail.com>
2022-02-21 16:34:51 +1100

    Update document ready

--- BP_custom.js
+++ BP_custom.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$( document ).ready(function() {
     $('.CPageContainer').addClass('is-searchActiveMobile');
     $('.CHeaderSearch').addClass('is-mobileOpenActive').addClass('is-openMobile');
     $('.CHeaderSearch-inner input').attr('placeholder', 'Search for pet products');
